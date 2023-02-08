ac21dc6aadca6bfae7909f28675722a20472f917
ethan <egroen7926@econestogac.on.ca>
2023-02-08 13:36:15 -0800

    finished rectangle requirements

--- PROG71020-Group7-Lab3/rectangle.cpp
+++ PROG71020-Group7-Lab3/rectangle.cpp
@@ -1,1 +1,48 @@
 #include "rectangle.h"
+
+Rectangle::Rectangle() {
+	length = 0;
+	width = 0;
+}
+
+Rectangle::Rectangle(int l, int w) {
+	length = l;
+	width = w;
+}
+
+int Rectangle::Area() {
+	int area = this->length * this->width;
+	return area;
+}
+
+Rectangle& Rectangle::setSides(int l, int w) {
+	this->length = l;
+	this->width = w;
+	return *this;
+}
+
+int Rectangle::getLength() {
+	return this->length;
+}
+
+int Rectangle::getWidth() {
+	return this->width;
+}
+
+std::ostream& operator << (std::ostream& cout, Rectangle& r) {
+	cout << "Rectangle: The length = " << r.length << ", the width = " << r.width << " and the area = " << r.Area() << std::endl;
+}
+
+Rectangle operator +(Rectangle r1, Rectangle r2) {
+	Rectangle r;
+	r.length = r1.length + r2.length;
+	r.width = r1.length + r2.length;
+	return r;
+}
+
+Rectangle operator *(Rectangle r1, int num) {
+	Rectangle r;
+	r.length = r1.length * num;
+	r.width = r1.width * num;
+	return r;
+}
--- PROG71020-Group7-Lab3/rectangle.h
+++ PROG71020-Group7-Lab3/rectangle.h
@@ -6,7 +6,29 @@
 // Rectangle header file
 
 #pragma once
-class rectangle
-{
-};
+#include <iostream>
+#include <math.h>
 
+using namespace std;
+
+class Rectangle
+{
+private:
+	int length;
+	int width;
+public:
+	Rectangle();
+	Rectangle(int l, int w);
+
+	int getLength();
+	int getWidth();
+	Rectangle& setSides(int l, int w);
+
+	int Area();
+
+	friend Rectangle operator +(Rectangle r1, Rectangle r2);
+
+	friend Rectangle operator *(Rectangle r1, int num);
+
+	friend std::ostream& operator << (std::ostream& cout, Rectangle& r);
+};
